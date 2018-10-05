4609803e953926e0670e59335a75624f74388e2a
felix <lysongfei@gmail.com>
2018-10-05 22:34:55 +0800

    change to maven

---
+++ src/main/java/it/unimi/dsi/fastutil/objects/ObjectList.java
@@ -0,0 +1,118 @@
+
+/*
+	* Copyright (C) 2002-2017 Sebastiano Vigna
+	*
+	* Licensed under the Apache License, Version 2.0 (the "License");
+	* you may not use this file except in compliance with the License.
+	* You may obtain a copy of the License at
+	*
+	*     http://www.apache.org/licenses/LICENSE-2.0
+	*
+	* Unless required by applicable law or agreed to in writing, software
+	* distributed under the License is distributed on an "AS IS" BASIS,
+	* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	* See the License for the specific language governing permissions and
+	* limitations under the License.
+	*/
+
+
+package it.unimi.dsi.fastutil.objects;
+
+import java.util.List;
+
+
+
+/** A type-specific {@link List}; provides some additional methods that use polymorphism to avoid (un)boxing.
+	*
+	* <p>Note that this type-specific interface extends {@link Comparable}: it is expected that implementing
+	* classes perform a lexicographical comparison using the standard operator "less then" for primitive types,
+	* and the usual {@link Comparable#compareTo(Object) compareTo()} method for objects.
+	*
+	* <p>Additionally, this interface strengthens {@link #listIterator()},
+	* {@link #listIterator(int)} and {@link #subList(int,int)}.
+	*
+	* <p>Besides polymorphic methods, this interfaces specifies methods to copy into an array or remove contiguous
+	* sublists. Although the abstract implementation of this interface provides simple, one-by-one implementations
+	* of these methods, it is expected that concrete implementation override them with optimized versions.
+	*
+	* @see List
+	*/
+
+public interface ObjectList <K> extends List<K>, Comparable<List<? extends K>>, ObjectCollection <K> {
+	/** Returns a type-specific iterator on the elements of this list.
+	 *
+	 * <p>Note that this specification strengthens the one given in {@link List#iterator()}.
+	 * It would not be normally necessary, but {@link java.lang.Iterable#iterator()} is bizarrily re-specified
+	 * in {@link List}.
+	 *
+	 * @return an iterator on the elements of this list.
+	 */
+	@Override
+	ObjectListIterator <K> iterator();
+
+	/** Returns a type-specific list iterator on the list.
+	 *
+	 * @see List#listIterator()
+	 */
+	@Override
+	ObjectListIterator <K> listIterator();
+
+	/** Returns a type-specific list iterator on the list starting at a given index.
+	 *
+	 * @see List#listIterator(int)
+	 */
+	@Override
+	ObjectListIterator <K> listIterator(int index);
+
+	/** Returns a type-specific view of the portion of this list from the index {@code from}, inclusive, to the index {@code to}, exclusive.
+	 *
+	 * <p>Note that this specification strengthens the one given in {@link List#subList(int,int)}.
+	 *
+	 * @see List#subList(int,int)
+	 */
+	@Override
+	ObjectList <K> subList(int from, int to);
+
+	/** Sets the size of this list.
+	 *
+	 * <p>If the specified size is smaller than the current size, the last elements are
+	 * discarded. Otherwise, they are filled with 0/{@code null}/{@code false}.
+	 *
+	 * @param size the new size.
+	 */
+
+	void size(int size);
+
+	/** Copies (hopefully quickly) elements of this type-specific list into the given array.
+	 *
+	 * @param from the start index (inclusive).
+	 * @param a the destination array.
+	 * @param offset the offset into the destination array where to store the first element copied.
+	 * @param length the number of elements to be copied.
+	 */
+	void getElements(int from, Object a[], int offset, int length);
+
+	/** Removes (hopefully quickly) elements of this type-specific list.
+	 *
+	 * @param from the start index (inclusive).
+	 * @param to the end index (exclusive).
+	 */
+	void removeElements(int from, int to);
+
+	/** Add (hopefully quickly) elements to this type-specific list.
+	 *
+	 * @param index the index at which to add elements.
+	 * @param a the array containing the elements.
+	 */
+	void addElements(int index, K a[]);
+
+	/** Add (hopefully quickly) elements to this type-specific list.
+	 *
+	 * @param index the index at which to add elements.
+	 * @param a the array containing the elements.
+	 * @param offset the offset of the first element to add.
+	 * @param length the number of elements to add.
+	 */
+	void addElements(int index, K a[], int offset, int length);
+}
+
